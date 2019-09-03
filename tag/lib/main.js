11571a2cb7af934c135c05a09372f1faf0e73943
Jonatan Hjalmarsson <jonatan@atellus.se>
2019-09-03 12:27:55 +0200

    Bump

--- tag/lib/main.js
+++ tag/lib/main.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -12,11 +13,12 @@ const github = require('@actions/github');
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const myInput = core.getInput('myInput');
-            core.debug(`Hello ${myInput} from inside a container`);
+            let myInput = core.getInput('myInput');
+            console.log(`Hello ${myInput} from inside a container`);
             // Get github context data
             const context = github.context;
             console.log(`We can even get context data, like the repo: ${context.repo.repo}`);
+            core.setOutput('myOutput', myInput.replace('e', 'E').replace('refs/tags/', ''));
         }
         catch (error) {
             core.setFailed(error.message);
--- tag/src/main.ts
+++ tag/src/main.ts
@@ -3,12 +3,14 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput} from inside a container`);
+    let myInput = core.getInput('myInput');
+    console.log(`Hello ${myInput} from inside a container`);
 
     // Get github context data
     const context = github.context;
     console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+
+    core.setOutput('myOutput', myInput.replace('e', 'E').replace('refs/tags/', ''));
   } catch (error) {
     core.setFailed(error.message);
   }
