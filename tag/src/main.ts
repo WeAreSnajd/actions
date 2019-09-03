692608a1d2f9405603d621391533302ce09b49d7
Jonatan Hjalmarsson <jonatan@atellus.se>
2019-09-03 12:34:10 +0200

    Export env var snajdTag

--- tag/src/main.ts
+++ tag/src/main.ts
@@ -11,6 +11,7 @@ async function run() {
     console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
 
     core.setOutput('myOutput', myInput.replace('e', 'E').replace('refs/tags/', ''));
+    core.exportVariable('snajdTag', myInput.replace('e', 'E').replace('refs/tags/', ''));
   } catch (error) {
     core.setFailed(error.message);
   }
